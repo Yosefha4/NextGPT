4b3390f805d35f66b769d4b3de70b6274ee1182f
Yosef Haimjan <yosef050468@gmail.com>
2024-09-16 11:33:01 +0300

    Clerk setup

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ const HomePage = () => {
       <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="text-6xl font-bold text-neutral">Next GPT</h1>
-          <p className="py-6 text-lg leading-loose">
+          <p className="py-6 text-lg leading-loose ">
             Next GPT: Your AI language companion. Powered by OpenAI, it enhances your conversations, content creation, and more!
           </p>
           <Link href="/chat" className="btn btn-neutral">
